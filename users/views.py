629e3e351f8695e5eeab20912dc33566faf4e15c
MdNazmul9 <nazmul.cse.pust@gmail.com>
2021-10-19 13:52:04 +0600

    #User Login auth_view

--- users/views.py
+++ users/views.py
@@ -7,4 +7,15 @@ from django.contrib.auth import authenticate
 def home_view(request):
     return render(request, 'main.html', {})
 
+def auth_view(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            request.session['pk'] = user.pk
+            return redirect()
+    return render(request, 'auth.html', {'form': form})
+
 
