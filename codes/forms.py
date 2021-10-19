a7ff462ce9ab481e3cc7028e5c042b9be0761aae
MdNazmul9 <nazmul.cse.pust@gmail.com>
2021-10-19 13:26:29 +0600

    # CodeForm

---
+++ codes/forms.py
@@ -0,0 +1,9 @@
+from django.db.models import forms
+from .models import Code
+
+class CodeForm(forms.ModelForm):
+
+    number = forms.CharField(label='Code', help_text='Enter SMS verification codes')
+    class Meta:
+        model = Code
+        fields = ('number',)
