5bd15f878fcfabd557602c843d7a732a429c5e09
MdNazmul9 <nazmul.cse.pust@gmail.com>
2021-10-19 13:43:57 +0600

    # User login home_view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
-# Create your views here.
+@login_required
+def home_view(request):
+    return render(request, 'main.html', {})
+
+
